49e2042f19d35822a98797a2c678e00c9e327d91
kunal mittal <kanumittal99@gmail.com>
2019-11-17 00:39:19 +0530

    Added create request function

--- controllers/camp_control.js
+++ controllers/camp_control.js
@@ -4,7 +4,7 @@ const Tx = require('ethereumjs-tx').Transaction;
 const web3js = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/7386bdf0b20e48db9a9d4eb445bb1803"));
 const compiledCamp=require('../build/Campaign.json');
 const contractABIc=JSON.parse(compiledCamp.interface);
-var contractAddress='0xFC7f44bCBd0381A524f3Bd839E5dAc4aDf80D589';
+var contractAddress='0x21fb434dc27c1ebcc701e0FcE9e532c8aF8aD35e';
 module.exports={    
     getDeployedCampaignd(req,res,next){
         var address=req.params.address;
@@ -30,7 +30,7 @@ module.exports={
         web3js.eth.getTransactionCount('0xb9bEb78AFD25A0a26E1fc6501e23E70F1B010259').then(function(v){
         count=v;
         var value=req.body.value;
-        var address=req.body.address;
+        var address=req.params.address;
         var contract=new web3js.eth.Contract(contractABIc,address);
         var privateKey = Buffer.from('ba69725568ff6674053b638b5a964ada3e7c5e0ef7d26f3b751d7faf9d5f9898', 'hex');
         var rawT={"from":0xb9bEb78AFD25A0a26E1fc6501e23E70F1B010259,"to":address,"value":web3js.utils.toHex(value),"gasPrice":web3js.utils.toHex(20* 1e9),"gasLimit":web3js.utils.toHex(3100000),"data":contract.methods.contribute().encodeABI(),"nonce":web3js.utils.toHex(count)};
@@ -48,11 +48,53 @@ module.exports={
         web3js.eth.getTransactionCount('0xb9bEb78AFD25A0a26E1fc6501e23E70F1B010259').then(function(v){
         count=v;
         var description=req.body.description;
-        var addressc=req.body.address;
+        var addressc=req.params.address;
         var recipient=req.body.recipient;
         var value=req.body.value;
+        var contract=new web3js.eth.Contract(contractABIc,addressc);
         var privateKey = Buffer.from('ba69725568ff6674053b638b5a964ada3e7c5e0ef7d26f3b751d7faf9d5f9898', 'hex');
-        var rawT={"from":0xb9bEb78AFD25A0a26E1fc6501e23E70F1B010259,"to":address,"value":web3js.utils.toHex(value),"gasPrice":web3js.utils.toHex(20* 1e9),"gasLimit":web3js.utils.toHex(3100000),"data":contract.methods.contribute(createRequest(description,value,recipient)).encodeABI(),"nonce":web3js.utils.toHex(count)};
+        var rawT={"from":0xb9bEb78AFD25A0a26E1fc6501e23E70F1B010259,"to":addressc,"value":"0x0","gasPrice":web3js.utils.toHex(20* 1e9),"gasLimit":web3js.utils.toHex(3100000),"data":contract.methods.createRequest(description,value,recipient).encodeABI(),"nonce":web3js.utils.toHex(count)};
+        var transaction = new Tx(rawT,{chain:'rinkeby', hardfork: 'petersburg'});            
+        transaction.sign(privateKey);
+        web3js.eth.sendSignedTransaction('0x'+transaction.serialize().toString('hex'))
+                    .then(function(transactionHash){
+                        res.send(transactionHash);
+                    });
+        });
+    },
+
+    getAllRequest(req,res,next){
+        var address=req.params.address;
+        var contract=new web3js.eth.Contract(contractABIc,address);
+        var arr=[];
+        var i;
+        contract.methods.getRequestsCount().call()
+            .then(function(length){    
+                var func=async()=>{
+                    for(i=0;i<length;i++){
+                        var data=await contract.methods.requests(i).call()
+                        arr.push(data);
+                    }
+                    return arr;
+                }
+                func().then((arrd)=>{
+                    res.send(arrd);
+                });
+            })
+            .catch(function(err){
+                res.send(err);
+        })
+    },
+
+    approveRequest(req,res,next){
+        var count;
+        web3js.eth.getTransactionCount('0xb9bEb78AFD25A0a26E1fc6501e23E70F1B010259').then(function(v){
+        count=v;
+        var index=req.params.index;
+        var addressc=req.params.address;
+        var contract=new web3js.eth.Contract(contractABIc,addressc);
+        var privateKey = Buffer.from('ba69725568ff6674053b638b5a964ada3e7c5e0ef7d26f3b751d7faf9d5f9898', 'hex');
+        var rawT={"from":0xb9bEb78AFD25A0a26E1fc6501e23E70F1B010259,"to":addressc,"value":"0x0","gasPrice":web3js.utils.toHex(20* 1e9),"gasLimit":web3js.utils.toHex(3100000),"data":contract.methods.approveRequest(index).encodeABI(),"nonce":web3js.utils.toHex(count)};
         var transaction = new Tx(rawT,{chain:'rinkeby', hardfork: 'petersburg'});            
         transaction.sign(privateKey);
         web3js.eth.sendSignedTransaction('0x'+transaction.serialize().toString('hex'))
--- routes/camp_route.js
+++ routes/camp_route.js
@@ -2,6 +2,7 @@ const {app}=require('../admin.js');
 const control=require('../controllers/camp_control.js');
 module.exports=(app)=>{
     app.get('/getd/:address',control.getDeployedCampaignd);
-    app.post('/contribute',control.contributeC);
-    app.post('/createrequest',control.createRequest);
+    app.get('/getallr/:address',control.getAllRequest);
+    app.post('/contribute/:address',control.contributeC);
+    app.post('/createrequest/:address',control.createRequest);
 }
