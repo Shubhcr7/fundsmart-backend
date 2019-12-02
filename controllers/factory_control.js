91012aed9b6e612bca22e52a540ab941f837f82a
Shubhcr7 <shubhamsharmacr7@gmail.com>
2019-12-02 19:01:40 +0530

    check-9

--- controllers/factory_control.js
+++ controllers/factory_control.js
@@ -2,6 +2,7 @@ const Web3 = require('web3');
 const http = require('http');
 const _=require('lodash');
 const Tx = require('ethereumjs-tx');
+const fs = require('fs-extra');
 const web3js = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/7386bdf0b20e48db9a9d4eb445bb1803"));
 const compiledCampf = require('../build/CampaignFactory.json');
 const contractABI = JSON.parse(compiledCampf.interface);
@@ -43,12 +44,20 @@ module.exports = {
                             var idea = req.body.idea;
                             var prod_desc = req.body.prod_desc;
                             var proj_type = req.body.proj_type;
+                            var prod_images1=req.body.prod_images1;
                             var privateKey = Buffer.from('ba69725568ff6674053b638b5a964ada3e7c5e0ef7d26f3b751d7faf9d5f9898', 'hex');
                             var rawT = { "from": 0xb9bEb78AFD25A0a26E1fc6501e23E70F1B010259, "to": contractAddress, "value": "0x0", "gasPrice": web3js.utils.toHex(20 * 1e9), "gasLimit": web3js.utils.toHex(2100000), "data": contract.methods.createCampaign(goal,minimum, name, about, idea, prod_desc, proj_type).encodeABI(), "nonce": web3js.utils.toHex(count) };
                             var transaction = new Tx(rawT, { chain: 'rinkeby', hardfork: 'petersburg' });
                             transaction.sign(privateKey);
                             web3js.eth.sendSignedTransaction('0x' + transaction.serialize().toString('hex'))
                                 .then(function (transactionHash) {
+                                    if(transactionHash.status==true){
+                                        var newPath='../images/name.jpg';
+                                        fs.rename(prod_images1,newPath, function (err) {
+                                            if (err) throw err
+                                            console.log('Successfully renamed - AKA moved!')
+                                          })
+                                    }
                                     res.send(transactionHash);
                                 });
                         });
