e2cd4df7b5d77daa46e3a3d3e8bc2adabf0321eb
Shubhcr7 <shubhamsharmacr7@gmail.com>
2019-12-02 15:58:12 +0530

    number problem

--- controllers/factory_control.js
+++ controllers/factory_control.js
@@ -36,15 +36,13 @@ module.exports = {
                         var count;
                         web3js.eth.getTransactionCount('0xb9bEb78AFD25A0a26E1fc6501e23E70F1B010259').then(function (v) {
                             count = v;
-                            var goal = (req.body.goal*Math.pow(10,18)).toString();
-                            var minimum = (req.body.funds*Math.pow(10,18)).toString();
                             var name = namec;
                             var about = req.body.about;
                             var idea = req.body.idea;
                             var prod_desc = req.body.prod_desc;
                             var proj_type = req.body.proj_type;
                             var privateKey = Buffer.from('ba69725568ff6674053b638b5a964ada3e7c5e0ef7d26f3b751d7faf9d5f9898', 'hex');
-                            var rawT = { "from": 0xb9bEb78AFD25A0a26E1fc6501e23E70F1B010259, "to": contractAddress, "value": "0x0", "gasPrice": web3js.utils.toHex(20 * 1e9), "gasLimit": web3js.utils.toHex(2100000), "data": contract.methods.createCampaign(goal,minimum, name, about, idea, prod_desc, proj_type).encodeABI(), "nonce": web3js.utils.toHex(count) };
+                            var rawT = { "from": 0xb9bEb78AFD25A0a26E1fc6501e23E70F1B010259, "to": contractAddress, "value": "0x0", "gasPrice": web3js.utils.toHex(20 * 1e9), "gasLimit": web3js.utils.toHex(2100000), "data": contract.methods.createCampaign((req.body.goal*Math.pow(10,18)).toString(),(req.body.funds*Math.pow(10,18)).toString(), name, about, idea, prod_desc, proj_type).encodeABI(), "nonce": web3js.utils.toHex(count) };
                             var transaction = new Tx(rawT, { chain: 'rinkeby', hardfork: 'petersburg' });
                             transaction.sign(privateKey);
                             web3js.eth.sendSignedTransaction('0x' + transaction.serialize().toString('hex'))
