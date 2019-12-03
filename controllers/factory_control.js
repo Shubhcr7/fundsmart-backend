f0202f33c500dac1139cf5bd0658ac32941b93ad
Shubhcr7 <shubhamsharmacr7@gmail.com>
2019-12-03 21:59:55 +0530

    check-32

--- controllers/factory_control.js
+++ controllers/factory_control.js
@@ -262,7 +262,7 @@ module.exports = {
                 }
                 func().then((arrd) => {
                     arrd = _.find(arrd,function(o){
-                        return o.name.indexOf(namec);
+                        return o.name.indexOf(namec)>-1;
                     });
                     if(arrd==undefined){
                         res.send('error');
